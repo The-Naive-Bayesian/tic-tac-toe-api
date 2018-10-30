74bc7198082323e323f60fba7ee8f04fe4ecdc44
Chris Emerson <chris.emerson@roledex.com>
2018-10-29 23:24:31 -0600

    Add basic placeholder for request
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const express = require('express');
 const port = 4200;
 const app = express();
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send({
         message: 'hello, world'
     });
+});
+
+app.post('/tic-tac-toe', (req, res) => {
+    // example return value
+    res.send([
+        [{fill: 'black'}, null, null],
+        [null, {fill: 'red'}, {fill: 'red'}],
+        [null, null, {fill: 'black'}],
+    ]);
 });
 
 app.listen(port, () => {
